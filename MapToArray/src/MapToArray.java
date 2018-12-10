9662b3a3d61e133930e9fc0247a53f6f1643fb58
Rajasekhar Reddy <Rajasekhar Reddy@DESKTOP-LMUN7FU>
2018-12-10 19:09:06 +0530

    project commited

---
+++ MapToArray/src/MapToArray.java
@@ -0,0 +1,29 @@
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapToArray {
+
+	public static void main(String[] args) {
+
+		HashMap<Integer, String> hashMap = new HashMap<>();
+		hashMap.put(101, "abc");
+		hashMap.put(102, "def");
+		hashMap.put(103, "ghi");
+		hashMap.put(104, "jkl");
+		MapToArray mapToArray = new MapToArray();
+		String[] convertMapToArray = mapToArray.convertMapToArray(hashMap);
+		for (String values : convertMapToArray) {
+			System.out.println(values);
+		}
+
+	}
+
+	public String[] convertMapToArray(Map<Integer, String> sourceMap) {
+
+		Collection<String> values = sourceMap.values();
+		String[] targetArray = values.toArray(new String[values.size()]);
+		return targetArray;
+
+	}
+}
